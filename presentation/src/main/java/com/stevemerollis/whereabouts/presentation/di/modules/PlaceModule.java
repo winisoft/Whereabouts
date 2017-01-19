f1f59f36cf89a206cac0d8296ae7f9529b2d47fc
winisoft <stevemerollis@gmail.com>
2017-01-19 18:56:55 -0500

    Wrong comment

--- presentation/src/main/java/com/stevemerollis/whereabouts/presentation/di/modules/PlaceModule.java
+++ presentation/src/main/java/com/stevemerollis/whereabouts/presentation/di/modules/PlaceModule.java
@@ -3,7 +3,7 @@ package com.stevemerollis.whereabouts.presentation.di.modules;
 import dagger.Module;
 
 /**
- * Dagger module that provides user related collaborators.
+ * Dagger module that provides place related collaborators.
  */
 @Module
 public class PlaceModule {
