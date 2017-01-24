9ee01502014c5eec559b3a93a6a7c47309cfbd6b
winisoft <stevemerollis@gmail.com>
2017-01-23 21:28:53 -0500

    Documentation

--- presentation/src/main/java/com/stevemerollis/whereabouts/presentation/util/ResourcesUtil.java
+++ presentation/src/main/java/com/stevemerollis/whereabouts/presentation/util/ResourcesUtil.java
@@ -5,6 +5,12 @@ import android.util.TypedValue;
 
 public class ResourcesUtil {
 
+    /**
+     * Get a float value from resources -- Android implements this slightly differently
+     * @param context The context from which we're using this util
+     * @param resId The resource ID of the dimen we wish to use
+     * @return A float representing a resource value
+     */
     public static float getFloat(Context context, int resId){
 
         TypedValue outValue = new TypedValue();
